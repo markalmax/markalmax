cd4032a4dc9e158c29b87b0a4ee41bdc7c2c5686
markalmax <markalmax782@gmail.com>
2026-03-02 01:46:43 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,18 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let name = "<span style='color: red;'>markalmax</span>";
+</script>
+
+<h1>Welcome to {@html name}</h1>
+<p>Uhhhhhhhhhhhhhhhhhh</p>
+
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Arsenal:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+    h1 {
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
+    p {
+        font-family: 'Arsenal', sans-serif;
+        width: 115%;
+    }
+</style>
