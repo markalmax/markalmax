f881bcc12c4c2add9ee737ea1d5131ce5930c3e3
markalmax <markalmax782@gmail.com>
2026-03-02 10:41:06 +0200

    BG

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,37 @@
     let name = "<span style='color: red;'>markalmax</span>";
 </script>
 
-<h1>Welcome to {@html name}</h1>
+<h1>WELCOME TO {@html name.toUpperCase()}</h1>
 <p>Uhhhhhhhhhhhhhhhhhh</p>
 
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Arsenal:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+    @font-face {
+        font-family: "Lust";
+        src: url("/fonts/lust.otf") format("opentype");
+    }
+    @font-face {
+        font-family: "LustDisplay";
+        src: url("/fonts/lust-display.otf") format("opentype");
+    }
+    :global(body){
+        background-image: url("/bg.png");
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+        margin: 0;
+        padding: 0;
+        height: 100vh;
+        width: 100vw;
+        overflow: hidden;
+    }
+    :global(html){
+        height: 100%;
+        width: 100%;
+    }
     h1 {
-        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        font-family: "LustDisplay", sans-serif;
     }
     p {
         font-family: 'Arsenal', sans-serif;
